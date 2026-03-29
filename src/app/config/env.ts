9c3c398c5838feeb63643cb68a83f517278a7b78
Uttam <uttamrohit4545@gmail.com>
2026-03-30 00:56:40 +0600

    feat(error): add custom AppError class

---
+++ src/app/config/env.ts
@@ -0,0 +1,61 @@
+import dotenv from "dotenv";
+import status from "http-status";
+import AppError from "../errorHelpers/AppError";
+
+dotenv.config();
+
+interface EnvConfig {
+  NODE_ENV: string;
+  PORT: string;
+  DATABASE_URL: string;
+
+  // Cloudinary
+  CLOUDINARY_CLOUD_NAME: string;
+  CLOUDINARY_API_KEY: string;
+  CLOUDINARY_API_SECRET: string;
+
+  // Stripe
+  STRIPE_SECRET_KEY: string;
+  STRIPE_PUBLISHABLE_KEY: string;
+}
+
+const loadEnvVariables = (): EnvConfig => {
+  const requiredEnvVariables = [
+    "NODE_ENV",
+    "PORT",
+    "DATABASE_URL",
+
+    // Cloudinary
+    "CLOUDINARY_CLOUD_NAME",
+    "CLOUDINARY_API_KEY",
+    "CLOUDINARY_API_SECRET",
+
+    // Stripe
+    "STRIPE_SECRET_KEY",
+    "STRIPE_PUBLISHABLE_KEY",
+  ];
+
+  requiredEnvVariables.forEach((variable) => {
+    if (!process.env[variable]) {
+      throw new AppError(
+        status.INTERNAL_SERVER_ERROR,
+        `Environment variable ${variable} is required but not set in .env file.`
+      );
+    }
+  });
+
+  return {
+    NODE_ENV: process.env.NODE_ENV as string,
+    PORT: process.env.PORT as string,
+    DATABASE_URL: process.env.DATABASE_URL as string,
+
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string,
+
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY as string,
+    STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY as string,
+  };
+};
+
+export const envVars = loadEnvVariables();
